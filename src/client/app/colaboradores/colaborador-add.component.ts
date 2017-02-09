5f104916f175c8f6ee89b933fc379e6432e751ae
ralb05 <ralb05@gmail.com>
2017-02-09 13:15:06 -0500

    add colaboradores

--- src/client/app/colaboradores/colaborador-add.component.ts
+++ src/client/app/colaboradores/colaborador-add.component.ts
@@ -1,13 +1,15 @@
 /**
  * Created by TracesMaker on 08/02/2017.
  */
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
-import { Location }                 from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ColaboradoresService } from './colaboradoresservice';
 import { Colaborador } from './colaboradores';
+import { Location }                 from '@angular/common';
 import 'rxjs/add/operator/switchMap';
 import {Observable} from 'rxjs/Observable';
+import {date} from "gulp-util";
+import {modelGroupProvider} from "@angular/forms/src/directives/ng_model_group";
 
 export class PrimeColaborador implements Colaborador {
     constructor(public idColaborador?, public numeroDocumento?, public primerNombre?, public fechaDesde?, public cargoActual?, public tipoDocumento?, public Avatar?, public ciudadExpedicion?, public fechaExp?, public fechaNacimiento?, public idtercero?, public ciudadNacimiento?, public nacionalidad?, public genero?, public estadoCivil?, public factorrh?, public numeroDeHijos?, public lateralidad?, public nivelEducativo?, public profesion?, public estratoSocioEconomico?, public vivienda?, public vehiculo?, public tallaCamisa?, public tallaPantalon?, public tallaCalzado?, public fechaDeste?) {}
@@ -28,27 +30,23 @@ export class ColaboradorAddComponent {
 
     constructor(
         private colaboradorService: ColaboradoresService,
-        private router: ActivatedRoute,
-        private location: Location
+        private router: Router
     ) {}
 
     addColaborador() {
         /*this.colaboradorService.addColaborador(this.colaborador);
         this.location.back();*/
-        this.colaboradorService.addColaborador(this.model)
+
+        this.colaboradorService.addColaborador(this.colaborador)
             .subscribe(
                 data => {
-                    this.location.back();
-                    //this.router.navigate(['/colaboradores']);
+                    this.router.navigate(['/colaboradores']);
                 },
                 error => {
-                    /*this.alertService.error(error);
-                    this.loading = false;*/
                 });
     }
 
     goBack(): void {
-        this.location.back();
-        //this.router.navigate(['/colaboradores']);
+        this.router.navigate(['/colaboradores']);
     }
 }
