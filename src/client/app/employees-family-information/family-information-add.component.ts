3c4956dababd830a8ce3f2f7e287413b77e2b118
ralb05 <ralb05@gmail.com>
2017-02-27 11:13:39 -0500

    validaciones de fecha y rangos de fecha
    se valido el rango de fecha si se selecciona cc
    se restringió el enter en el formulario

--- src/client/app/employees-family-information/family-information-add.component.ts
+++ src/client/app/employees-family-information/family-information-add.component.ts
@@ -9,6 +9,7 @@ import {SelectItem, ConfirmationService, Message} from 'primeng/primeng';
 import {FormBuilder, FormGroup, Validators, FormControl, NgForm } from '@angular/forms';
 import {Router}  from '@angular/router';
 import {Location}  from '@angular/common';
+import * as moment from 'moment/moment';
 
 @Component({
     moduleId: module.id,
@@ -32,6 +33,9 @@ export class FamilyInformationAddComponent implements OnInit {
     familyform: FormGroup;
 
     submitted: boolean;
+    maxDate:Date = null;
+    es: any;
+    range: string;
 
     constructor(
         private familyInformationService: FamilyInformationService,
@@ -43,6 +47,16 @@ export class FamilyInformationAddComponent implements OnInit {
 
     ngOnInit() {
 
+        //let now = new Date();
+        //this.maxDateValue = `${now.getMonth()+1}/${now.getDate()}/${now.getFullYear()}`;
+      this.es = {
+        firstDayOfWeek: 1,
+        dayNames: [ "domingo","lunes","martes","miércoles","jueves","viernes","sábado" ],
+        dayNamesShort: [ "dom","lun","mar","mié","jue","vie","sáb" ],
+        dayNamesMin: [ "D","L","M","X","J","V","S" ],
+        monthNames: [ "enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre" ],
+        monthNamesShort: [ "ene","feb","mar","abr","may","jun","jul","ago","sep","oct","nov","dic" ]
+      };
         this.familyform = this.fb.group({
             'tipoDeDocumento': new FormControl('', Validators.required),
             'numeroDeDocumento': new FormControl('', Validators.required),
@@ -66,6 +80,18 @@ export class FamilyInformationAddComponent implements OnInit {
         );
         this.familyInformation.segundoApellido = this.familyInformation.segundoNombre = '';
 
+      let today = new Date();
+      let month = today.getMonth();
+      let year = today.getFullYear();
+      let prevMonth = (month === 0) ? 11 : month -1;
+      let prevYear = (prevMonth === 11) ? year - 1 : year;
+      let nextMonth = (month === 11) ? 0 : month + 1;
+      let lasYear = year-50;
+      this.maxDate = new Date();
+      this.maxDate.setMonth(month);
+      this.maxDate.setFullYear(year);
+      this.range = `${lasYear}:${year}`;
+
     }
 
     onSubmit(value: string) {
@@ -81,7 +107,6 @@ export class FamilyInformationAddComponent implements OnInit {
                 error => {
                 });
     }
-
 
     goBack(): void {
         this.confirmationService.confirm({
@@ -101,8 +126,71 @@ export class FamilyInformationAddComponent implements OnInit {
         let d = new Date(Date.parse(event));
         this.familyInformation.fechadeNacimiento = `${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()}`;
     }
-    onChangeMethod(event) {
-      //alert('sd');
+
+    onBlurMethod(event) {
+      let inp = event.target.value;
+      try{
+        inp = Date.parse(inp);
+      }catch (err){
+        inp = "";
+      }
+
+      if(inp!= "" && inp != null && !isNaN(inp)) {
+        let d = new Date(inp);
+        this.familyInformation.fechadeNacimiento = `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
+      }else{
+        this.familyInformation.fechadeNacimiento = '';
+      }
     }
+
+    onChangeMethod(event) {
+      let today = new Date();
+      let month = today.getMonth();
+      let year = today.getFullYear();
+      let prev18Year =  year - 18;
+      let lastYear =  prev18Year - 50;
+      this.maxDate = new Date();
+
+
+      if(event.value==1 || event.value == 2){
+          this.maxDate.setMonth(month);
+          this.maxDate.setFullYear(prev18Year);
+          this.range = `${lastYear}:${prev18Year}`;
+      }else{
+        this.maxDate.setMonth(month);
+        this.maxDate.setFullYear(year);
+        this.range = `${prev18Year}:${year}`;
+      }
+      if((this.familyInformation.fechadeNacimiento)== null || (this.familyInformation.fechadeNacimiento)== "" ){
+        this.familyInformation.fechadeNacimiento = `${this.maxDate.getMonth()+1}/${this.maxDate.getDate()}/${this.maxDate.getFullYear()}`
+      }
+
+    }
+    keyPressOnForm(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+      }
+    }
+
+  strToDate(newDateString: string): Date {
+    if (newDateString) {
+      let mom: moment.Moment = moment(newDateString, 'MM/DD/YYYY');
+      if (mom.isValid()) {
+        return mom.toDate();
+      }
+    }
+    return null;
+  }
+
+  dateToStr(newDate: Date, format?: string): string {
+    if (newDate && moment(newDate).isValid()) {
+      if (format) {
+        return moment(newDate).format(format);
+      }
+      return moment(newDate).format('MM/DD/YYYY');
+    }
+    // date vide ou incorrecte
+    return '';
+  }
 
 }
--- src/client/app/employees-family-information/family-information-update.component.ts
+++ src/client/app/employees-family-information/family-information-update.component.ts
@@ -29,6 +29,9 @@ export class FamilyInformationUpdateComponent implements OnInit{
     familyform: FormGroup;
 
     submitted: boolean;
+    maxDate:Date = null;
+    es: any;
+    range: string;
 
     constructor(
         private familyInformationService: FamilyInformationService,
@@ -39,6 +42,23 @@ export class FamilyInformationUpdateComponent implements OnInit{
 
     ) {}
     ngOnInit(): void {
+      this.es = {
+        firstDayOfWeek: 1,
+        dayNames: [ "domingo","lunes","martes","miércoles","jueves","viernes","sábado" ],
+        dayNamesShort: [ "dom","lun","mar","mié","jue","vie","sáb" ],
+        dayNamesMin: [ "D","L","M","X","J","V","S" ],
+        monthNames: [ "enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre" ],
+        monthNamesShort: [ "ene","feb","mar","abr","may","jun","jul","ago","sep","oct","nov","dic" ]
+      };
+        let today = new Date();
+        let month = today.getMonth();
+        let year = today.getFullYear();
+        let last18Year = year-18;
+        let lastYear = year-50;
+        this.maxDate = new Date();
+        this.maxDate.setMonth(month);
+        this.maxDate.setFullYear(year);
+        this.range = `${lastYear}:${year}`;
 
         this.familyform = this.fb.group({
           'tipoDeDocumento': new FormControl('', Validators.required),
@@ -68,6 +88,10 @@ export class FamilyInformationUpdateComponent implements OnInit{
                     this.familyInformation = familyInformation;
                     this.selectedDocument = this.familyInformation.tipoDeDocumento.value;
                     this.selectedRelationship = this.familyInformation.parentesco.value;
+                    if(this.selectedDocument==1 || this.selectedDocument == 2){
+                      this.maxDate.setFullYear(last18Year);
+                      this.range = `${lastYear}:${last18Year}`;
+                    }
 
                 });
 
@@ -102,9 +126,54 @@ export class FamilyInformationUpdateComponent implements OnInit{
         });
     }
 
-    onSelectMethod(event) {
-        let d = new Date(Date.parse(event));
-        this.familyInformation.fechadeNacimiento = `${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()}`;
+  onSelectMethod(event) {
+    let d = new Date(Date.parse(event));
+    this.familyInformation.fechadeNacimiento = `${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()}`;
+  }
+
+  onBlurMethod(event) {
+    let inp = event.target.value;
+    try{
+      inp = Date.parse(inp);
+    }catch (err){
+      inp = "";
     }
+
+    if(inp!= "" && inp != null && !isNaN(inp)) {
+      let d = new Date(inp);
+      this.familyInformation.fechadeNacimiento = `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
+    }else{
+      this.familyInformation.fechadeNacimiento = '';
+    }
+  }
+
+  onChangeMethod(event) {
+    let today = new Date();
+    let month = today.getMonth();
+    let year = today.getFullYear();
+    let prev18Year =  year - 18;
+    let lastYear =  prev18Year - 50;
+    this.maxDate = new Date();
+
+
+    if(event.value==1 || event.value == 2){
+      this.maxDate.setMonth(month);
+      this.maxDate.setFullYear(prev18Year);
+      this.range = `${lastYear}:${prev18Year}`;
+    }else{
+      this.maxDate.setMonth(month);
+      this.maxDate.setFullYear(year);
+      this.range = `${prev18Year}:${year}`;
+    }
+    if((this.familyInformation.fechadeNacimiento)== null || (this.familyInformation.fechadeNacimiento)== "" ){
+      this.familyInformation.fechadeNacimiento = `${this.maxDate.getMonth()+1}/${this.maxDate.getDate()}/${this.maxDate.getFullYear()}`
+    }
+
+  }
+  keyPressOnForm(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+    }
+  }
 
 }
