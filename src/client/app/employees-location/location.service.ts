dc34b38b3c6a0bb8f052c87b88662742c787a401
robertochajin <roberto.chajin@ciberdix.com>
2017-03-09 11:48:44 -0500

    Eliminando el fake del service

--- src/client/app/employees-location/location.service.ts
+++ src/client/app/employees-location/location.service.ts
@@ -1,52 +1,56 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { EmployeesLocation } from './employees-location';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class LocationService {
 
-    constructor(private http: Http) {}
+  api_url:string = "http://40.71.92.147:9095";
 
-    getAll()  {
-        return this.http.get('/api/employees-location')
-            .map((res:Response) => res.json().data);
-    }
+  constructor(private http:Http) {
+  }
 
-    getPrincipalNomenclatureList() {
-        return this.http.get('/api/principal-nomenclature')
-            .map((res:Response) => res.json().data);
-    }
+  getAll() {
+    return this.http.get(this.api_url + '/employeesLocations')
+      .map((res:Response) => res.json().data);
+  }
 
-    getComplementaryNomenclatureList() {
-        return this.http.get('/api/complementary-nomenclature')
-            .map((res:Response) => res.json().data);
-    }
+  getPrincipalNomenclatureList() {
+    return this.http.get(this.api_url + '/principalNomenclature')
+      .map((res:Response) => res.json().data);
+  }
 
-    getAddressTypeList() {
-        return this.http.get('/api/address-types')
-            .map((res:Response) => res.json().data);
-    }
+  getComplementaryNomenclatureList() {
+    return this.http.get(this.api_url + '/complementaryNomenclature')
+      .map((res:Response) => res.json().data);
+  }
 
-    getAllCities(qr: any)  {
-        return this.http.get('/api/cities/s/'+ qr).map((res:Response) => res.json().data);
-    }
+  getAddressTypeList() {
+    return this.http.get(this.api_url + '/addressTypes')
+      .map((res:Response) => res.json().data);
+  }
 
-    add(f: EmployeesLocation) {
-        return this.http.post('/api/employees-location',f).map((res:Response) => res.json());
-    };
+  getAllCities(qr:any) {
+    return this.http.get(this.api_url + '/cities/s/' + qr).map((res:Response) => res.json().data);
+  }
 
-    update(f: EmployeesLocation) {
-        return this.http.put('/api/employees-location/'+ f.idUbicacion,f).map((res:Response) => res.json());
-    }
+  add(f:EmployeesLocation) {
+    return this.http.post(this.api_url + '/employeesLocations', f).map((res:Response) => res.json());
+  };
 
-    get(id: number) {
-        return this.http.get('/api/employees-location/'+ id)
-               .map((res:Response) => res.json().data as EmployeesLocation);
-    }
+  update(f:EmployeesLocation) {
+    return this.http.put(this.api_url + '/employeesLocations/' + f.idUbicacion, f).map((res:Response) => res.json());
+  }
 
-    delete(f: EmployeesLocation) {
-        const respuesta =  this.http.delete('/api/employees-location/'+ f.idUbicacion);
-        return respuesta.map((res:Response) => res.json());
-    }
+  get(id:number) {
+    return this.http.get(this.api_url + '/employeesLocations/' + id)
+      .map((res:Response) => res.json().data as EmployeesLocation);
+  }
+
+  delete(f:EmployeesLocation) {
+    const respuesta = this.http.delete(this.api_url + '/employees-location/' + f.idUbicacion);
+    return respuesta.map((res:Response) => res.json());
+  }
 
 }
