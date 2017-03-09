001f0960c6d7895fa35863f33f4ae1f6c49e0737
robertochajin <roberto.chajin@ciberdix.com>
2017-03-09 17:41:00 -0500

    Obtiene el ID del colaborador en el momento de la carga inicial para consumir el service getAllByEmployees y mostrar las ubicaciones registradas

--- src/client/app/employees-location/location.component.ts
+++ src/client/app/employees-location/location.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { LocationService } from './location.service';
@@ -6,58 +6,61 @@ import { EmployeesLocation } from './employees-location';
 import {ConfirmationService} from 'primeng/primeng';
 
 @Component({
-    moduleId: module.id,
-    templateUrl: 'location.component.html',
-    selector: 'employees-locations',
-    providers:  [ConfirmationService]
+  moduleId: module.id,
+  templateUrl: 'location.component.html',
+  selector: 'employees-locations',
+  providers: [ConfirmationService]
 })
 export class LocationComponent implements OnInit {
 
-    employeesLocations: EmployeesLocation[];
-    dialogObjet: EmployeesLocation = new EmployeesLocation();
-    constructor(
-              private locationService: LocationService,
-              private router: Router,
-              private confirmationService: ConfirmationService
-            ) {}
+  @Input() colaborador:number;
 
-    ngOnInit() {
-        this.locationService.getAll().subscribe(
-            employeesLocations => this.employeesLocations = employeesLocations
-        );
-    }
+  employeesLocations:EmployeesLocation[];
+  dialogObjet:EmployeesLocation = new EmployeesLocation();
 
-    delete(l: EmployeesLocation) {
-        this.locationService.delete(l);
-        this.employeesLocations.splice(this.employeesLocations.indexOf(l), 1);
-        l = null;
-    }
-    del(f: EmployeesLocation) {
-      this.dialogObjet = f;
-      this.confirmationService.confirm({
-        message: ` ¿Esta seguro que desea eliminar?`,
-        header: 'Corfirmación',
-        icon: 'fa fa-question-circle',
-        accept: () => {
-          this.locationService.delete(this.dialogObjet);
-          this.employeesLocations.splice(this.employeesLocations.indexOf(this.dialogObjet), 1);
-          this.dialogObjet = null;
-        },
-        reject: () => {
+  constructor(private locationService:LocationService,
+              private router:Router,
+              private confirmationService:ConfirmationService) {
+  }
 
-        }
-      });
-    }
+  ngOnInit() {
+    this.locationService.getAllByEmployee(this.colaborador).subscribe(
+      employeesLocations => this.employeesLocations = employeesLocations
+    );
+  }
 
-    detail(l: EmployeesLocation) {
-        this.router.navigate(['employees-location/detail/'+l.idUbicacion]);
-    }
+  delete(l:EmployeesLocation) {
+    this.locationService.delete(l);
+    this.employeesLocations.splice(this.employeesLocations.indexOf(l), 1);
+    l = null;
+  }
 
-    add() {
-        this.router.navigate(['employees-location/add']);
-    }
+  del(f:EmployeesLocation) {
+    this.dialogObjet = f;
+    this.confirmationService.confirm({
+      message: ` ¿Esta seguro que desea eliminar?`,
+      header: 'Corfirmación',
+      icon: 'fa fa-question-circle',
+      accept: () => {
+        this.locationService.delete(this.dialogObjet);
+        this.employeesLocations.splice(this.employeesLocations.indexOf(this.dialogObjet), 1);
+        this.dialogObjet = null;
+      },
+      reject: () => {
 
-    update(l: EmployeesLocation) {
-        this.router.navigate(['employees-location/update/'+l.idUbicacion]);
-    }
+      }
+    });
+  }
+
+  detail(l:EmployeesLocation) {
+    this.router.navigate(['employees-location/detail/' + l.idUbicacion]);
+  }
+
+  add() {
+    this.router.navigate(['employees-location/add']);
+  }
+
+  update(l:EmployeesLocation) {
+    this.router.navigate(['employees-location/update/' + l.idUbicacion]);
+  }
 }
