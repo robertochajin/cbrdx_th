d779d71339c1570c5dc8ade0ef08ad63df6276ae
tracesmaker <javiersk81@gmail.com>
2017-03-13 11:51:08 -0500

    Actualizacion lateralidad
    Actualizacion lateralidad

--- src/client/app/employees/employees.service.ts
+++ src/client/app/employees/employees.service.ts
@@ -8,30 +8,31 @@ export class EmployeesService {
 
 
     //au = API_URL;
-    //au = 'http://localhost:8445/';  para cambios rápidos locales
-    au = 'http://40.71.92.147:8445/'; //dominio se debe cambiar a negocio.
+    //au = 'http://localhost:8446/terceros';  //para cambios rápidos locales
+  //au = 'http://40.71.92.147:8446/teceros'; // dominio se debe cambiar a negocio.
+
     headers = new Headers({'Content-Type': 'application/json'});
 
     constructor(private http: Http) {}
 
     getAll()  {
-        return this.http.get(this.au+'employees').map((res:Response) => res.json());
+        return this.http.get(this.au).map((res:Response) => res.json());
     }
 
     add(c: Employee) {
-        return this.http.post(this.au+'employees',c).map((res:Response) => res.json());
+        return this.http.post(this.au,c).map((res:Response) => res.json());
     };
 
     update(c: Employee) {
-        return this.http.put(this.au+'employees/'+ c.idTercero,c).map((res:Response) => res.json());
+        return this.http.put(this.au+'/'+ c.idTercero,c).map((res:Response) => res.json());
     }
 
     get(id: number) {
-        return this.http.get(this.au+'employees/'+ id).map((res:Response) => res.json() as Employee);
+        return this.http.get(this.au+'/'+ id).map((res:Response) => res.json() as Employee);
     }
 
     delete(c: Employee) {
-        const respuesta =  this.http.delete(this.au+'employees/'+ c.idTercero);
+        const respuesta =  this.http.delete(this.au+'/'+ c.idTercero);
         return respuesta.map((res:Response) => res.json());
     }
 
