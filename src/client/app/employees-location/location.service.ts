228c0babfa1f060e589a18eb2e8d8cd43f7be0e0
robertochajin <roberto.chajin@ciberdix.com>
2017-03-13 20:36:15 -0500

    Ajuste de actualización y eliminación de las ubicaciones

--- src/client/app/employees-location/location.service.ts
+++ src/client/app/employees-location/location.service.ts
@@ -1,63 +1,63 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { EmployeesLocation } from './employees-location';
-import {API_URL} from "../global";
-import {Employee} from "../employees/employees";
+import { API_URL } from "../global";
+import { Employee } from "../employees/employees";
 
 @Injectable()
 export class LocationService {
 
-  constructor(private http:Http) {
+  constructor(private http: Http) {
   }
 
   getAllByEmployee(id: number) {
     return this.http.get(API_URL + '/employeesLocations/employees/' + id)
-      .map((res:Response) => res.json());
+      .map((res: Response) => res.json());
   }
 
   getPrincipalNomenclatureList() {
     return this.http.get(API_URL + '/nomenclatures/principal')
-      .map((res:Response) => res.json());
+      .map((res: Response) => res.json());
   }
 
   getComplementaryNomenclatureList() {
     return this.http.get(API_URL + '/nomenclatures/complementary')
-      .map((res:Response) => res.json());
+      .map((res: Response) => res.json());
   }
 
   getAddressTypeList() {
     return this.http.get(API_URL + '/nomenclatures/addressType')
-      .map((res:Response) => res.json());
+      .map((res: Response) => res.json());
   }
 
-  getAllCities(qr:any) {
+  getAllCities(qr: any) {
     return this.http.get(API_URL + '/divisionPolitica/cities/s/' + qr)
-      .map((res:Response) => res.json());
+      .map((res: Response) => res.json());
   }
 
-  getAllHoods(qr:any) {
+  getAllHoods(qr: any) {
     //
     return this.http.get(API_URL + '/divisionPolitica/hoods/s/' + qr)
-      .map((res:Response) => res.json());
+      .map((res: Response) => res.json());
   }
 
   add(f: any) {
     return this.http.post(API_URL + '/employeesLocations', f)
-      .map((res:Response) => res.json());
+      .map((res: Response) => res.json());
   };
 
-  update(f:EmployeesLocation) {
-    return this.http.put(API_URL + '/employeesLocations/' + f.idUbicacion, f)
-      .map((res:Response) => res.json());
+  update(f: any) {
+    return this.http.put(API_URL + '/employeesLocations/', f)
+      .map((res: Response) => res.json());
   }
 
-  get(id:number) {
+  get(id: number) {
     return this.http.get(API_URL + '/employeesLocations/location/' + id)
-      .map((res:Response) => res.json());
+      .map((res: Response) => res.json());
   }
 
-  delete(f:EmployeesLocation) {
-    const respuesta = this.http.delete(API_URL + '/employeesLocations/' + f.idUbicacion);
-    return respuesta.map((res:Response) => res.json());
+  delete(f: any) {
+    return this.http.put(API_URL + '/employeesLocations/', f)
+      .map((res: Response) => res.json());
   }
 }
