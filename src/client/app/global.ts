c9efb8147742904f10d471426e98a2d550c434a1
tracesmaker <javiersk81@gmail.com>
2017-03-15 15:12:28 -0500

    global azure
    global azure

--- src/client/app/global.ts
+++ src/client/app/global.ts
@@ -1,2 +1,6 @@
+//export const API_URL_D:string = 'http://localhost:8444';
+//export const API_URL:string = 'http://localhost:8448';
 //export const API_URL_D:string = 'http://192.168.253.37:8444'; // ip equipo crezcamos
 //export const API_URL:string = 'http://192.168.253.37:8448'; // ip equipo crezcamos
+export const API_URL_D:string = 'http://40.71.92.147:8444'; // ip azure
+export const API_URL:string = 'http://40.71.92.147:8448';  // ip azure
