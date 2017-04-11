8716d4a591281f6ba6cc1858010840b6cf25d7b8
robertochajin <roberto.chajin@ciberdix.com>
2017-04-11 17:04:17 -0500

    Pantalla de Cargos Personalidad
    Pantalla de Cargos Personalidad

---
+++ src/client/app/_services/list-position-personality.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers } from '@angular/http';
+import {AuthenticationService} from "./authentication.service";
+
+@Injectable()
+export class PositionPersonalityTypesServices {
+
+   public headers = new Headers({'Content-Type': 'application/json'});
+
+   private serviceURL = '<%= SVC_TH_URL %>/api/tiposPersonas';
+
+   constructor(private http: Http,
+               private authenticationService: AuthenticationService
+   ) {
+      this.headers = new Headers({'Content-Type': 'application/json', 'Authorization': this.authenticationService.token});
+   }
+
+   getAllEnabled()  {
+      return this.http.get(this.serviceURL,{headers: this.headers}).map((res:Response) => res.json());
+   }
+
+}
