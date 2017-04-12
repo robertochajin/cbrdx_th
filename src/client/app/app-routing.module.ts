b40aea5c97128c2ace8520fcf513fd883908293e
ralb05 <ralb05@gmail.com>
2017-04-12 16:44:50 -0500

    Adelanto de Unificación

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -130,8 +130,6 @@ import {CompanyAssetsComponent} from "./position-company-assets/company-assets.c
 import {AbsenceComponent} from "./position-absence/position-absence.component";
 import {PersonalityComponent} from "./position-personality/personality.component";
 
-//autoridades
-import {AuthoritiesComponent} from "./position-authorities/position-authorities.component";
 const routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
 
@@ -270,8 +268,6 @@ const routes = [
   {path: 'position-risk/:idCargo', component: RiskComponent, canActivate: [AuthGuard]},
   //cargos posición en caso de ausencia
   {path: 'position-absence/:idCargo', component: AbsenceComponent, canActivate: [AuthGuard]},
-   //cargos autoridades
-  {path: 'position-authorities/:idCargo', component: AuthoritiesComponent, canActivate: [AuthGuard]},
 
 
 ];
