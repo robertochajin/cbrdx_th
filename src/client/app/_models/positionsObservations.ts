a0eb6ad803229d8a44b67cf89a4272078011320e
ralb05 <ralb05@gmail.com>
2017-04-13 11:40:12 -0500

    Ajustes generales de Cargo

--- src/client/app/_models/positionsObservations.ts
+++ src/client/app/_models/positionsObservations.ts
@@ -3,9 +3,10 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class PositionsObservations {
 
-  public idObservacion : number;
-  public Observacion  : string;
+  public idCargoEstadoObservacion : number;
+  public observacion  : string;
   public idCargo  : number;
+  public idEstadoCargo  : number;
   public indicadorHabilitado : boolean;
   public auditoriaUsuario: number;
   public auditoriaFecha: string;
