5f4ab3bb63f1b3257bf0a60e86c4b8abfeaca842
robertochajin <roberto.chajin@ciberdix.com>
2017-04-24 23:08:28 -0500

    Ajustes Angular JWT
    Ajustes Angular JWT

--- src/client/app/shared/toolbar/toolbar.component.ts
+++ src/client/app/shared/toolbar/toolbar.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { JwtHelper} from 'angular2-jwt';
+import {Component} from '@angular/core';
+import {JwtHelper} from 'angular2-jwt';
 
 /**
  * This class represents the toolbar component.
  */
 @Component({
-  moduleId: module.id,
-  selector: 'sd-toolbar',
-  templateUrl: 'toolbar.component.html',
-  styleUrls: ['toolbar.component.css']
+   moduleId: module.id,
+   selector: 'sd-toolbar',
+   templateUrl: 'toolbar.component.html',
+   styleUrls: ['toolbar.component.css']
 })
 export class ToolbarComponent {
 
@@ -16,10 +16,11 @@ export class ToolbarComponent {
 
    jwtHelper: JwtHelper = new JwtHelper();
 
-   constructor(){
+   constructor() {
       let token = localStorage.getItem('token');
 
-      this.usuarioLogueado = this.jwtHelper.decodeToken(token);
+      if (token != null)
+         this.usuarioLogueado = this.jwtHelper.decodeToken(token);
    }
 }
 
