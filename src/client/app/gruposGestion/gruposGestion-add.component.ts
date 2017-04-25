a3ceefdd9a0fa4cc201b51a7782096df913e9902
TracesMaker <javiersk81@gmail.com>
2017-04-24 19:19:03 -0500

    issue 337

--- src/client/app/gruposGestion/gruposGestion-add.component.ts
+++ src/client/app/gruposGestion/gruposGestion-add.component.ts
@@ -20,6 +20,7 @@ export class GruposGestionAddComponent implements OnInit {
     displayDialog: boolean = false;
     isRequired = false;
     isGreater = true;
+   private es: any;
 
     constructor(private gruposGestionService: GruposGestionService, private router: Router) {
         gruposGestionService.listGruposGestion().subscribe(res => {
@@ -29,6 +30,14 @@ export class GruposGestionAddComponent implements OnInit {
 
     ngOnInit(): void {
 
+       this.es = {
+          firstDayOfWeek: 1,
+          dayNames: ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
+          dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
+          dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
+          monthNames: ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
+          monthNamesShort: ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic']
+       };
     }
 
     validateGreater() {
