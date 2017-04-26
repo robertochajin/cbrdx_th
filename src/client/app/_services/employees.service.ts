5c4719b8edd9b42f6ac2f501d557a31396380479
ralb05 <ralb05@gmail.com>
2017-04-26 18:35:32 -0500

    Dashboard

--- src/client/app/_services/employees.service.ts
+++ src/client/app/_services/employees.service.ts
@@ -56,7 +56,6 @@ export class EmployeesService {
     validateDocument(numeroDocumento: string, idTipoDocumento: number) {
       return this.http.get(this.serviceURL+'terceros/'+ numeroDocumento+'/'+ idTipoDocumento+'/' ,{headers: this.headers}).map((res:Response) => res.json() as Employee);
       
-      
       /*.map((res:Response) => {
          //if (res.text() != '') {
             res.json() as Employee
