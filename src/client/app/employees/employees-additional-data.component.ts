af868f52a6176a6dfc997590786c4db94c0fe212
weymar <w1andresv@gmail.com>
2017-05-04 21:07:36 -0500

    datos de contacto

--- src/client/app/employees/employees-additional-data.component.ts
+++ src/client/app/employees/employees-additional-data.component.ts
@@ -1,123 +1,138 @@
 import "rxjs/add/operator/switchMap";
-import {Component, Input} from "@angular/core";
-import {Router, ActivatedRoute, Params} from "@angular/router";
-import {Location} from "@angular/common";
-import {Employee} from "../_models/employees";
-import {SelectItem, Message} from "primeng/primeng";
-import {EmployeesService} from "../_services/employees.service";
-import {ListEmployeesService} from "../_services/lists-employees.service";
-import {DivisionPolitica} from "../_models/divisionPolitica";
-import {NavService} from "../_services/_nav.service";
-import {ListaItem} from "../_models/listaItem";
-import {ListaService} from "../_services/lista.service";
+import { Component, Input } from "@angular/core";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { Location } from "@angular/common";
+import { Employee } from "../_models/employees";
+import { SelectItem, Message } from "primeng/primeng";
+import { EmployeesService } from "../_services/employees.service";
+import { ListEmployeesService } from "../_services/lists-employees.service";
+import { DivisionPolitica } from "../_models/divisionPolitica";
+import { NavService } from "../_services/_nav.service";
+import { ListaItem } from "../_models/listaItem";
+import { ListaService } from "../_services/lista.service";
 
-@Component({
-  moduleId: module.id,
-  selector: 'employees-additional-data',
-  templateUrl: 'employees-additional-data.component.html',
-  providers: []
-})
+@Component( {
+   moduleId: module.id,
+   selector: 'employees-additional-data',
+   templateUrl: 'employees-additional-data.component.html',
+   providers: []
+} )
 
 export class EmployeesAdditionalDataComponent {
-  @Input() employee:Employee;
-  header: string = 'Datos Adicionales ';
-  
-  personTypes: SelectItem[] = [];
-  documentTypes: SelectItem[] = [];
-  resultExpeditionCity: DivisionPolitica[];
-  resultBirthPlace: DivisionPolitica[] = [];
+   @Input() employee: Employee;
+   header: string = 'Datos Adicionales ';
 
-  lateralityTypes: SelectItem[] = [];
-  listSizeShirt: SelectItem[] = [];
-  listSizePants: SelectItem[] = [];
-  listSizeFootwear: SelectItem[] = [];
+   personTypes: SelectItem[] = [];
+   documentTypes: SelectItem[] = [];
+   resultExpeditionCity: DivisionPolitica[];
+   resultBirthPlace: DivisionPolitica[] = [];
 
-  msgs: Message[] = [];
+   lateralityTypes: SelectItem[] = [];
+   listSizeShirt: SelectItem[] = [];
+   listSizePants: SelectItem[] = [];
+   listSizeFootwear: SelectItem[] = [];
+   size: boolean = true;
+   peso: boolean = true;
+   submitted: boolean = false;
+   msgs: Message[] = [];
 
-  constructor(private employeesService: EmployeesService,
-              private listaService: ListaService,
-              private route: ActivatedRoute,
-              private router: Router,
-              private location: Location,
-              private listEmployeesService: ListEmployeesService,
-              private _nav: NavService) {
+   constructor( private employeesService: EmployeesService,
+                private listaService: ListaService,
+                private route: ActivatedRoute,
+                private router: Router,
+                private location: Location,
+                private listEmployeesService: ListEmployeesService,
+                private _nav: NavService ) {
 
-     this.listaService.getMasterDetails('ListasLateralidades').subscribe(res => {
-        this.lateralityTypes.push({label: 'Seleccione', value: null});
-        res.map((s: ListaItem) => {
-           this.lateralityTypes.push({label: s.nombre, value: s.idLista});
-        });
-     });
-     this.listaService.getMasterDetailsStartsByCode('ListasTallas','CAM').subscribe(rest => {
-      this.listSizeShirt.push({label: "Seleccione", value: null});
-      for (let dp of rest) {
-        this.listSizeShirt.push({
-          label: dp.nombre,
-          value: dp.idLista
-        });
+      this.listaService.getMasterDetails( 'ListasLateralidades' ).subscribe( res => {
+         this.lateralityTypes.push( { label: 'Seleccione', value: null } );
+         res.map( ( s: ListaItem ) => {
+            this.lateralityTypes.push( { label: s.nombre, value: s.idLista } );
+         } );
+      } );
+      this.listaService.getMasterDetailsStartsByCode( 'ListasTallas', 'CAM' ).subscribe( rest => {
+         this.listSizeShirt.push( { label: "Seleccione", value: null } );
+         for ( let dp of rest ) {
+            this.listSizeShirt.push( {
+               label: dp.nombre,
+               value: dp.idLista
+            } );
+         }
+      } );
+      this.listaService.getMasterDetailsStartsByCode( 'ListasTallas', 'ZAPA' ).subscribe( rest => {
+         this.listSizeFootwear.push( { label: "Seleccione", value: null } );
+         for ( let dp of rest ) {
+            this.listSizeFootwear.push( {
+               label: dp.nombre,
+               value: dp.idLista
+            } );
+         }
+      } );
+
+
+   }
+
+   ngOnInit() {
+      this.submitted = true;
+      let tipo = this.employee.genero == "Masculino" ? "PANH" : "PANM"
+      this.listaService.getMasterDetailsStartsByCode( 'ListasTallas', tipo ).subscribe( rest => {
+         this.listSizePants.push( { label: "Seleccione", value: null } );
+         for ( let dp of rest ) {
+            this.listSizePants.push( {
+               label: dp.nombre,
+               value: dp.idLista
+            } );
+         }
+      } );
+   }
+
+   onSubmit() {
+      this.msgs = [];
+      this.submitted = true;
+      if ( this.employee.peso === 0 ) {
+         this.peso = false;
+      } else {
+         if ( this.employee.talla === 0 ) {
+            this.size = false;
+         } else {
+            this.employeesService.update( this.employee )
+               .subscribe( data => {
+                  this.msgs.push( { severity: 'info', summary: 'Exito', detail: 'Registro guardado correctamente.' } );
+               }, error => {
+                  this.msgs.push( { severity: 'error', summary: 'Error', detail: 'Error al guardar.' } );
+               } );
+         }
       }
-    });
-     this.listaService.getMasterDetailsStartsByCode('ListasTallas','ZAPA').subscribe(rest => {
-      this.listSizeFootwear.push({label: "Seleccione", value: null});
-      for (let dp of rest) {
-        this.listSizeFootwear.push({
-          label: dp.nombre,
-          value: dp.idLista
-        });
+   }
+
+   getimc(): void {
+      if ( this.employee.peso !== 0 ) {
+         this.peso = true;
       }
-    });
-    
-    
-
-  }
-
-  ngOnInit() {
-    let tipo = this.employee.genero == "Masculino" ? "PANH":"PANM"
-     this.listaService.getMasterDetailsStartsByCode('ListasTallas',tipo).subscribe(rest => {
-      this.listSizePants.push({label: "Seleccione", value: null});
-      for (let dp of rest) {
-        this.listSizePants.push({
-          label: dp.nombre,
-          value: dp.idLista
-        });
+      if ( this.employee.talla !== 0 ) {
+         this.size = true;
       }
-    });
-  }
-
-  onSubmit() {
-    this.msgs = [];
-    this.employeesService.update(this.employee)
-      .subscribe(data => {
-        this.msgs.push({severity: 'info', summary: 'Exito', detail: 'Registro guardado correctamente.'});
-      }, error => {
-        this.msgs.push({severity: 'error', summary: 'Error', detail: 'Error al guardar.'});
-      });
-
-  }
-
-  getimc(): void {
-    if (this.employee.peso != null && this.employee.talla != null) {
-      if (!isNaN(this.employee.peso) && !isNaN(this.employee.talla)) {
-        var imc = (this.employee.peso / Math.pow((this.employee.talla / 100), 2));
-        this.employee.imc = Number(imc.toFixed(2));
+      if ( this.employee.peso != null && this.employee.talla != null ) {
+         if ( !isNaN( this.employee.peso ) && !isNaN( this.employee.talla ) ) {
+            var imc = (this.employee.peso / Math.pow( (this.employee.talla / 100), 2 ));
+            this.employee.imc = Number( imc.toFixed( 2 ) );
+         }
       }
-    }
-  }
 
-  inputNumberPeso() {
-    let peso = this.employee.peso+"";
-    if (this.employee.peso != null) {
-       if(peso==="0" ||peso===""){peso="1"}
-       this.employee.peso = Number(peso.replace(/[^0-9]/g, ''));
-    }
-  }
+   }
 
-  inputNumber() {
-    let size = this.employee.talla+"";
-    if (this.employee.talla != null) {
-       if(size==="0"||size===""){size="1"}
-      this.employee.talla = Number(size.replace(/[^0-9]/g, ''));
-    }
-  }
+   inputNumberPeso() {
+      let peso = this.employee.peso + "";
+      if ( this.employee.peso != null ) {
+         this.employee.peso = Number( peso.replace( /[^0-9]/g, '' ) );
+      }
+   }
+
+   inputNumber() {
+      let size = this.employee.talla + "";
+      if ( this.employee.talla != null ) {
+         this.employee.talla = Number( size.replace( /[^0-9]/g, '' ) );
+      }
+   }
 
 }
