51a035b30afe8c23bc4c319adb8a89e342be8edc
karenpintoromero <KarenPintoRomero@Claudias-MacBook-Pro.local>
2017-05-08 16:37:50 -0500

    Ortografía
    Cargos
    Riesgos

--- src/client/app/position-risks/position-risks.module.ts
+++ src/client/app/position-risks/position-risks.module.ts
@@ -5,11 +5,13 @@ import 'rxjs/add/operator/toPromise';
 import {RiskComponent} from "./position-risks.component";
 import {FormSharedModule } from '../shared/form-shared.module';
 import {RiskService} from '../_services/positios-risks.service';
+import {SharedModule} from '../shared/shared.module';
 import {InputSwitchModule} from 'primeng/primeng';
 
 @NgModule({
   imports: [
     FormSharedModule,
+    SharedModule,
     ReactiveFormsModule,
     InputSwitchModule
   ],
