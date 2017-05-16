f6e8e1f8f12aaddc781f7fd3a2b42ab118a4d347
weymar <w1andresv@gmail.com>
2017-05-16 08:46:32 -0500

    gestion de vacantes

--- src/client/app/assignment-professional/assignment-professional-component.ts
+++ src/client/app/assignment-professional/assignment-professional-component.ts
@@ -4,6 +4,8 @@ import { ActividadEconomicaService } from '../_services/actividadEconomica.servi
 import { Message, SelectItem } from 'primeng/primeng';
 import { NavService } from '../_services/_nav.service';
 import { Employee } from '../_models/employees';
+import { ListaService } from '../_services/lista.service';
+import { ListaItem } from '../_models/listaItem';
 
 @Component( {
                moduleId: module.id,
@@ -14,13 +16,22 @@ export class AssignmentProfessionalComponent {
 
    msg: Message;
    employee: Employee = new Employee();
+   listCategory: SelectItem[] = [];
 
    constructor( private router: Router,
       private actividadEconomicaService: ActividadEconomicaService,
+      private listaService: ListaService,
       private navService: NavService ) {
 
    }
-
+   ngOnInit() {
+      this.listaService.getMasterDetails( 'ListasClasificacionesSedes' ).subscribe( res => {
+         this.listCategory.push( { label: 'Seleccione', value: null } );
+         res.map( ( s: ListaItem ) => {
+            this.listCategory.push( { label: s.nombre, value: s.idLista } );
+         } );
+      } );
+   }
    goBack(): void {
    }
 
