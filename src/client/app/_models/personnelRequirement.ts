80ecc34a74ed6610609b92f527af118c28435e34
TracesMaker <javiersk81@gmail.com>
2017-05-16 09:15:48 -0500

    mdelo

--- src/client/app/_models/personnelRequirement.ts
+++ src/client/app/_models/personnelRequirement.ts
@@ -3,19 +3,30 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class PersonnelRequirement {
 
-   public idRequerimiento : number ;
-   public idSolicitante : number;
-   public motivo : string;
-   public idCargo : number;
-   public idFormaContratacion : number;
-   public idTipoContratacion : number;
-   public cantidadVacantes : number;
-   public cantidadConvocados : number;
-   public idEstado : number;
-   public auditoriaUsuario : number ;
-   public auditoriaFecha : string ;
-   public idResponsableSeleccion : number;
-   public idFormaRecluta : number;
+   public idRequerimiento: number;
+   public idSolicitante: number;
+   public justificacion: string;
+   public idCargo: number;
+   public idFormaContratacion: number;
+   public idTipoContratacion: number;
+   public cantidadVacantes: number;
+   public cantidadConvocados: number;
+   public idEstado: number;
+   public auditoriaUsuario: number;
+   public auditoriaFecha: string;
+   public idResponsableSeleccion: number;
+   public idFormaReclutamiento: number;
+   public idEstructuraOrganizacional: number;
+   public idZona: number;
+   public idEstructuraFisica: number;
+   public fechaSolicitud: string;
+   public indicadorAutorizacion: boolean;
+   public indicadorAumentoPlazas: boolean;
+   public idJefe: number;
+   public idCategoria: number;
+   public idTipoSolicitud: number;
+   public fechaInicio: string;
+   public fechaFin: string;
 
    constructor() {
    }
