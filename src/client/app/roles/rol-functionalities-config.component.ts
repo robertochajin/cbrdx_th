dcaae0fda656d1f8be9b1a2968c31bb13a8a6f50
ralb05 <ralb05@gmail.com>
2017-06-29 14:05:53 -0500

    ajuste en el configurador de formularios

--- src/client/app/roles/rol-functionalities-config.component.ts
+++ src/client/app/roles/rol-functionalities-config.component.ts
@@ -85,8 +85,8 @@ export class RolFuncionalitiesConfigComponent implements OnInit {
    cosntrucObj() {
       this.lfControles.map( ( s: any ) => {
          this.fControles = new RolFunctionalityControl();
-         if ( this.listaFuncionalityControl.find( d => d.idFuncionalidadControl === s.idFuncionalidadControl ) ) {
-            this.fControles = this.listaFuncionalityControl.find( d => d.idFuncionalidadControl = s.idFuncionalidadControl );
+         if ( this.listaFuncionalityControl.filter( d => d.idFuncionalidadControl === s.idFuncionalidadControl ).length > 0 ) {
+            this.fControles = this.listaFuncionalityControl.filter( d => d.idFuncionalidadControl === s.idFuncionalidadControl )[0];
             this.fControles.codigo = s.codigo;
          } else {
             this.fControles.idRolFuncionalidadControl = null;
@@ -95,9 +95,9 @@ export class RolFuncionalitiesConfigComponent implements OnInit {
             this.fControles.rol = this.rolFuncionality.rol;
             this.fControles.control = s.control;
             this.fControles.codigo = s.codigo;
-            this.fControles.indicadorHabilitado = false;
-            this.fControles.indicadorEditar = false;
-            this.fControles.indicadorSeccion = false;
+            this.fControles.indicadorHabilitado = true;
+            this.fControles.indicadorEditar = true;
+            this.fControles.indicadorSeccion = s.indicadorSeccion;
          }
          if ( s.indicadorSeccion === true ) {
             this.secciones.push( this.fControles );
