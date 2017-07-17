a39f3fec87f96cf22490277f87233b8b17ab3985
ralb05 <ralb05@gmail.com>
2017-07-17 10:47:47 -0500

    carpeta supplies

---
+++ src/client/app/supplies/supplies.module.ts
@@ -0,0 +1,30 @@
+import 'rxjs/add/operator/toPromise';
+import { NgModule } from '@angular/core';
+import { NavService } from '../_services/_nav.service';
+import { SharedModule } from '../shared/shared.module';
+import { FormSharedModule } from '../shared/form-shared.module';
+import { ListaService } from '../_services/lista.service';
+import { QuestionnairesComponent } from './supplies.component';
+import { QuestionnairesService } from '../_services/questionnaires.service';
+import { QuestionnairesAddComponent } from './supplies-add.component';
+import { QuestionnairesUpdateComponent } from './supplies-update.component';
+import { SolutionsQuestionnairesComponent } from './solutions-questionnaire/solutions-questionnaires.component';
+import { MasterAnswersService } from '../_services/masterAnswers.service';
+import { SolutionsQuestionnairesDetailComponent } from './solutions-questionnaire/solutions-questionnaires-detail.component';
+
+
+@NgModule( {
+              imports: [
+                 SharedModule,
+                 FormSharedModule,
+              ],
+
+              declarations: [ QuestionnairesComponent, QuestionnairesAddComponent, QuestionnairesUpdateComponent,
+                 SolutionsQuestionnairesComponent, SolutionsQuestionnairesDetailComponent
+              ],
+              bootstrap: [ QuestionnairesComponent ],
+              providers: [ QuestionnairesService, NavService, ListaService, MasterAnswersService ],
+              exports: [ QuestionnairesComponent, SolutionsQuestionnairesComponent, SolutionsQuestionnairesDetailComponent ]
+           } )
+export class QuestionnairesModule {
+}
