dc961815f2130cde830b6d02c97ca0bca7e81a72
TracesMaker <javiersk81@gmail.com>
2017-07-17 13:17:21 -0500

    Modulo de novedades

---
+++ src/client/app/novelties/novelties.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { FormSharedModule } from '../shared/form-shared.module';
+
+@NgModule( {
+              imports: [
+                 SharedModule,
+                 FormSharedModule
+              ],
+              declarations: [
+
+              ],
+              bootstrap: [ ],
+              providers: [ ],
+              exports: [  ]
+           } )
+export class NoveltiesModule {
+}
