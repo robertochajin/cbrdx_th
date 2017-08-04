aafc962fc736422cd686cb762d92a57788dad8e6
weymar <w1andresv@gmail.com>
2017-08-04 10:11:47 -0500

    tercero novedades estado jefe autoriza

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -218,6 +218,8 @@ import { SuppliesConfirmationComponent } from './employees/supplies-employees/su
 import { OccupationalExamsComponent } from './occupational-exams/occupational-exams.component';
 import { MedicalExamInformedConsentPeriodComponent } from './employees-clinical-information/informed-consent-period.component';
 import { AnswerExamsPeriodComponent } from './medical-institutions/answer-exams-period.component';
+import { AccidentIncidentComponent } from './accidents-incidents/accidents-incidents.component';
+import { AccidentIncidentPlanComponent } from './accidents-incidents/accidents-incidents-plan.component';
 
 const routes = [
    { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -542,7 +544,7 @@ const routes = [
    //  employees additional data
    { path: 'employees/supplies-confirmation/:id', component: SuppliesConfirmationComponent, canActivate: [ AuthGuard ] },
    // examenes ocupacionales
-   { path: 'occupational-exams', component: OccupationalExamsComponent }
+   { path: 'occupational-exams', component: OccupationalExamsComponent },
 
 
 
